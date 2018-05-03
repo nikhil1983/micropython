48191ac8662569cc75c967053584372e1b484556
Nikhil Komalan <34880960+nikhil1983@users.noreply.github.com>
2018-05-03 10:21:04 +0000

    SPI Pins changed, SD Card detect pin changed

--- ports/stm32/boards/STM32F4DISC/mpconfigboard.h
+++ ports/stm32/boards/STM32F4DISC/mpconfigboard.h
@@ -58,10 +58,11 @@
 #define MICROPY_HW_SPI1_SCK  (pin_A5)
 #define MICROPY_HW_SPI1_MISO (pin_A6)
 #define MICROPY_HW_SPI1_MOSI (pin_A7)
-#define MICROPY_HW_SPI2_NSS  (pin_B12)
-#define MICROPY_HW_SPI2_SCK  (pin_B13)
-#define MICROPY_HW_SPI2_MISO (pin_B14)
-#define MICROPY_HW_SPI2_MOSI (pin_B15)
+
+#define MICROPY_HW_SPI2_NSS  (pin_B14)
+#define MICROPY_HW_SPI2_SCK  (pin_B10)
+#define MICROPY_HW_SPI2_MISO (pin_C2)
+#define MICROPY_HW_SPI2_MOSI (pin_C3)
 
 // USRSW is pulled low. Pressing the button makes the input go high.
 #define MICROPY_HW_USRSW_PIN        (pin_A0)
@@ -84,6 +85,6 @@
 
 
 // SD card detect switch
-#define MICROPY_HW_SDCARD_DETECT_PIN        (pin_A8)
+#define MICROPY_HW_SDCARD_DETECT_PIN        (pin_B15)
 #define MICROPY_HW_SDCARD_DETECT_PULL       (GPIO_PULLUP)
 #define MICROPY_HW_SDCARD_DETECT_PRESENT    (GPIO_PIN_RESET)
