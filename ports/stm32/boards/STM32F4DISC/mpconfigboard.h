d9e8d9b57dc52f85ba5cc29a5940d0facf62d277
Nikhil Komalan <34880960+nikhil1983@users.noreply.github.com>
2018-05-14 06:10:02 +0000

    Update mpconfigboard.h

--- ports/stm32/boards/STM32F4DISC/mpconfigboard.h
+++ ports/stm32/boards/STM32F4DISC/mpconfigboard.h
@@ -59,7 +59,7 @@
 #define MICROPY_HW_SPI1_MISO (pin_A6)
 #define MICROPY_HW_SPI1_MOSI (pin_A7)
 
-#define MICROPY_HW_SPI2_NSS  (pin_B14)
+#define MICROPY_HW_SPI2_NSS  (pin_A4)
 #define MICROPY_HW_SPI2_SCK  (pin_B10)
 #define MICROPY_HW_SPI2_MISO (pin_C2)
 #define MICROPY_HW_SPI2_MOSI (pin_C3)
